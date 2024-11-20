7cf90abcb2cf8b787f7d39dd93d7c1faaeba4a16
tracy <tracy1997113@gmail.com>
2024-11-20 09:43:57 +0800

    add_contact(11/20 09:40)

--- obj/Debug/net8.0/shopping.AssemblyInfo.cs
+++ obj/Debug/net8.0/shopping.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shopping")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea90347a31abb05695be6ef4d2cc1e6d42275fa2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d95ee6622420b04abef08e96e6289f1bac505c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("shopping")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shopping")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
